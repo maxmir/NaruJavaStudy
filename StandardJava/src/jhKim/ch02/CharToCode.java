3c23d5b3510f85b5a20cf54d15a5fa9c53fcbb88
jhKim <maxmirjh@gmail.com>
2017-05-19 09:19:37 +0900

    2장 업데이트

---
+++ StandardJava/src/jhKim/ch02/CharToCode.java
@@ -0,0 +1,16 @@
+/**
+ * @author jhkim
+ * CharToCode.java
+ * 2017. 5. 18.
+ */
+package jhKim.ch02;
+
+public class CharToCode {
+    public static void main(String... args) {
+        char ch = 'A';
+        int code = (int)ch;
+        
+        System.out.println(ch);
+        System.out.println(code);
+    }
+}
